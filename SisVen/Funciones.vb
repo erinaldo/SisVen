4c999d1b15f840680c240dffdcfeb5845a10d175
Wikets Software <wiketssoftware@gmail.com>
2021-08-26 22:19:30 -0400

    bloqueados

--- SisVen/Funciones.vb
+++ SisVen/Funciones.vb
@@ -23,7 +23,7 @@ Module Funciones
     Public Const W_SELECCIONAR As String = "Seleccionar"
     Public Const c_Empresa As String = "Wikets Software"
     Public Const c_Sistema As String = "SisVen"
-    Public Const Version As String = "1.45 21/08/2021"
+    Public Const Version As String = "1.46 26/08/2021"
 
     Public gProgramador As Boolean = False
     Public gMonoLocal As Boolean = True
